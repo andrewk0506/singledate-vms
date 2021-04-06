30e7e1afdd532517a701411a0618eba40a22d8fd
Matthew Otto <mattmotto18@gmail.com>
2021-04-05 23:44:19 -0500

    making changes to be able to commit with the flake8 and black

---
+++ vms_app/models/role.py
@@ -0,0 +1,10 @@
+from django.db import models
+from .site import Site
+
+
+class Role(models.Model):
+
+    ROLES = [("A", "ADMIN"), ("V", "VACCINATOR"), ("S", "SUPPORT STAFF")]
+
+    role = models.CharField(max_length=1, choices=ROLES, default=None, null=True)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, null=True)
--- vms_app/models/staff.py
+++ vms_app/models/staff.py
@@ -1,15 +1,16 @@
 from django.db import models
+from .role import Role
+
 
 class Staff(models.Model):
-	
-	class Meta:
-		app_label = "vms_app"
+    class Meta:
+        app_label = "vms_app"
 
-	surName = models.CharField(max_length=100)
-	givenName = models.CharField(max_length=100)
-	phoneNumber = models.CharField(max_length=40)
-	email = models.CharField(max_length=60)
-	medicalId = models.DecimalField(decimal_places=0, max_digits=10)
-	HIPAACert = models.DateTimeField()
-	password = models.CharField(max_length=60)
-	role = models.CharField(max_length=15)
+    surName = models.CharField(max_length=100)
+    givenName = models.CharField(max_length=100)
+    phoneNumber = models.CharField(max_length=40)
+    email = models.CharField(max_length=60)
+    medicalId = models.DecimalField(decimal_places=0, max_digits=10)
+    HIPAACert = models.DateTimeField()
+    password = models.CharField(max_length=60)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
