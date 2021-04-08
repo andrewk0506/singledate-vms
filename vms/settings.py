f8535a5146d3728368be4842d2c10c08df0ce929
cmbayo01 <camille@Camilles-MacBook-Pro.local>
2021-04-08 11:41:27 -0700

    small changes before merge

--- vms/settings.py
+++ vms/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "dev"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0','localhost']
 
 STATIC_URL = '/static/'
 
