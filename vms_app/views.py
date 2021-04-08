1070a579e687140c93521b944e3055b0f6acd5c2
Akhil Ravipati <akhil.ravipati.o@gmail.com>
2021-04-08 13:27:02 -0400

    Use existing choices in the dose model

--- vms_app/views.py
+++ vms_app/views.py
@@ -1,41 +1,56 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Dose
+
+
 def index(request):
-	return render(request, "index.html", {})
+    return render(request, "index.html", {})
+
 
 def preregister(request):
-	return render(request, "preregister.html", {})
+    return render(request, "preregister.html", {})
+
 
 def check(request):
-	return render(request, "check.html", {})
+    return render(request, "check.html", {})
+
 
 def signup(request):
-	return render(request, "signup.html", {})
+    return render(request, "signup.html", {})
+
 
 def verify(request):
-	return render(request, "verify.html", {})
+    return render(request, "verify.html", {})
+
 
 def admin_login(request):
-	return render(request, "admin-login.html")
+    return render(request, "admin-login.html")
+
 
 def role_select(request):
-	return render(request, "role-selection.html")
+    return render(request, "role-selection.html")
+
 
 def staff_select(request):
-	return render(request, "select-staff.html")
+    return render(request, "select-staff.html")
+
 
 def appointments(request):
-	return render(request, "todays-appts.html")
+    return render(request, "todays-appts.html")
+
 
 def patient_info(request):
-	return render(request, "patient-info.html")
+    return render(request, "patient-info.html")
+
 
 def medical_questions(request):
-	return render(request, "medical-questions.html")
+    return render(request, "medical-questions.html")
+
 
 def next_appt(request):
-	return render(request, "nextappt.html")
+    return render(request, "nextappt.html")
+
 
 def vaccine_info(request):
-	return render(request, "vaccine-information.html")
+    context = {"locations": Dose.LOCATIONS}
+    return render(request, "vaccine-information.html", context)
