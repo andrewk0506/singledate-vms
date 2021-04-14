d707833fd1357343d736dd7610c60d0867e5299a
Bruk Zewdie <bbz2103@columbia.edu>
2021-04-14 00:29:41 -0400

    database settings mysql

--- vms/settings.py
+++ vms/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = "vms.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # "ENGINE": "django.db.backends.mysql",
-        # "OPTIONS": {
-        #     "read_default_file": "local.cnf",
-        # },
+        # "ENGINE": "django.db.backends.sqlite3",
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "local.cnf",
+        },
     }
 }
 # Password validation
