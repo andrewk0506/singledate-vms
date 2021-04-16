d3279dd3fe41d451c8b3bccf6e9e529b6e453faf
cmbayo <camille.mbayo@gmail.com>
2021-04-16 15:01:52 -0700

    fixed based url #77

--- vms/urls.py
+++ vms/urls.py
@@ -19,6 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('vms/', include('vms_app.urls')),
+    path('', include('vms_app.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
