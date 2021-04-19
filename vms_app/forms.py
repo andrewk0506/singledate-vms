ad84349ff5cce9b4f424305a367f6ae980077b5e
cmbayo <camille.mbayo@gmail.com>
2021-04-18 20:21:18 -0700

    change in forms

--- vms_app/forms.py
+++ vms_app/forms.py
@@ -27,7 +27,6 @@ class PatientForm(ModelForm):
                   'zip_code',
                   'city',
                   'state']
-                # exclude = ['race']
         fieldsets = (
             ('Personal Details', {
                 'fields': ('first_name',
@@ -47,9 +46,6 @@ class PatientForm(ModelForm):
                   'state')
             })
         )
-
-    # TODO: Add race as a question
-    #       Probably involves changing the model type
 
     gender = ChoiceField(choices=Gender.choices, widget=RadioSelect())
     race = MultipleChoiceField(choices=Race.choices, widget=CheckboxSelectMultiple())
